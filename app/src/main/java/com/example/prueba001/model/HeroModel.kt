8c2583f22fd29c1a1cd7c58a24bb4f28507bae30
merenapp <equijadaac@gmail.com>
2022-09-12 21:42:26 +0200

    Añadir biografía al modelo

--- app/src/main/java/com/example/prueba001/model/HeroModel.kt
+++ app/src/main/java/com/example/prueba001/model/HeroModel.kt
@@ -1,6 +1,7 @@
 package com.example.prueba001.model
 
 import com.example.prueba001.model.appearance.AppearanceModel
+import com.example.prueba001.model.biography.BiographyModel
 import com.example.prueba001.model.images.ImagesModel
 import com.example.prueba001.model.stats.StatsModel
 
@@ -8,5 +9,6 @@ data class HeroModel(
     var images: ImagesModel,
     var name: String,
     var powerstats: StatsModel,
-    var appearance: AppearanceModel
+    var appearance: AppearanceModel,
+    var biography: BiographyModel
 )
