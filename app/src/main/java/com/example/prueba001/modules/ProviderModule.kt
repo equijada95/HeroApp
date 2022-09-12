043a0b810678384b46b4dff3a0ccba70ea52082e
merenapp <equijadaac@gmail.com>
2022-09-12 22:25:41 +0200

    Creado ProviderModule

---
+++ app/src/main/java/com/example/prueba001/modules/ProviderModule.kt
@@ -0,0 +1,37 @@
+package com.example.prueba001.modules
+
+import com.example.prueba001.provider.HeroProvider
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ProviderModule {
+
+    @Provides
+    @Named("BaseUrl")
+    fun provideBaseUrl() = "https://akabab.github.io/superhero-api/api/".toHttpUrl()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(@Named("BaseUrl") baseUrl: HttpUrl): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providerNewsProvider(retrofit: Retrofit): HeroProvider =
+        retrofit.create(HeroProvider::class.java)
+
+}
