26f2374c20f82b38d1eef8487f8cf4929494d97f
merenapp <equijadaac@gmail.com>
2022-09-13 19:49:16 +0200

    Corrección de modelo

--- app/src/main/java/com/example/prueba001/model/appearance/AppearanceModel.kt
+++ app/src/main/java/com/example/prueba001/model/appearance/AppearanceModel.kt
@@ -3,7 +3,7 @@ package com.example.prueba001.model.appearance
 data class AppearanceModel(
     var gender: String,
     var race: String,
-    var heigth: List<String>,
+    var height: List<String>,
     var weight: List<String>,
     var eyeColor: String,
     var hairColor: String
