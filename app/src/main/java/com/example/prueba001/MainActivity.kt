f3a017bdf03dbda5b422d13802bae06121f21a63
merenapp <equijadaac@gmail.com>
2022-09-13 20:38:12 +0200

    Añadir fragment a la actividad

--- app/src/main/java/com/example/prueba001/MainActivity.kt
+++ app/src/main/java/com/example/prueba001/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.prueba001
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.prueba001.databinding.ActivityMainBinding
+import com.example.prueba001.fragments.list.ListFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        inflateView()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(binding.container.id, ListFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
+
+    private fun inflateView() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
 }
