880e8344af927245bc937267700059129dfa63e5
merenapp <equijadaac@gmail.com>
2022-09-15 22:25:11 +0200

    Creado DataBaseRepository

---
+++ app/src/main/java/com/example/prueba001/bbdd/repository/DataBaseRepository.kt
@@ -0,0 +1,30 @@
+package com.example.prueba001.bbdd.repository
+
+import androidx.lifecycle.LiveData
+import com.example.prueba001.bbdd.dao.HeroDao
+import com.example.prueba001.bbdd.models.HeroDbModel
+import javax.inject.Inject
+
+interface DataBaseRepository {
+
+    suspend fun getHerosFromDataBase(): LiveData<List<HeroDbModel?>?>?
+    suspend fun insertHero(hero: HeroDbModel)
+    suspend fun deleteHero(hero: HeroDbModel)
+
+}
+
+class DataBaseRepositoryImpl @Inject constructor(
+    private val dao: HeroDao
+) : DataBaseRepository {
+
+    override suspend fun getHerosFromDataBase(): LiveData<List<HeroDbModel?>?>? = dao.getAll()
+
+    override suspend fun insertHero(hero: HeroDbModel) {
+        dao.insert(hero)
+    }
+
+    override suspend fun deleteHero(hero: HeroDbModel) {
+        dao.delete(hero)
+    }
+
+}
