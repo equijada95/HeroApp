ed317a4da11d170eb56c470112218698b97a53ba
merenapp <equijadaac@gmail.com>
2022-09-15 22:26:17 +0200

    Añadir a favoritos desde detalle

--- app/src/main/java/com/example/prueba001/fragments/detail/DetailFragment.java
+++ app/src/main/java/com/example/prueba001/fragments/detail/DetailFragment.java
@@ -7,19 +7,28 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.example.prueba001.R;
+import com.example.prueba001.bbdd.models.HeroDbModel;
+import com.example.prueba001.bbdd.viewmodel.DataBaseViewModel;
 import com.example.prueba001.databinding.FragmentDetailBinding;
 import com.example.prueba001.model.HeroModel;
 
-public class DetailFragment extends Fragment {
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class DetailFragment extends Fragment implements View.OnClickListener {
 
     private HeroModel hero;
 
     private FragmentDetailBinding binding;
+
+    private DataBaseViewModel viewModel;
 
     public static DetailFragment newInstance(HeroModel hero) {
         DetailFragment fr = new DetailFragment();
@@ -37,10 +46,19 @@ public class DetailFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        viewModel = new ViewModelProvider(this).get(DataBaseViewModel.class);
         createViews();
     }
 
     private void createViews() {
+        if (hero.isFavorite()) {
+            binding.ivFav.setBackground(AppCompatResources.getDrawable(getContext(), R.drawable.ic_fav));
+        } else {
+            binding.ivFav.setBackground(AppCompatResources.getDrawable(getContext(), R.drawable.ic_unfav));
+        }
+
+        binding.ivFav.setOnClickListener(this);
+
         if (hero.getImages() != null) {
             Glide.with(getContext())
                     .load(hero.getImages().getLg())
@@ -95,4 +113,22 @@ public class DetailFragment extends Fragment {
         }
     }
 
+    private void setFav() {
+        if (hero.isFavorite()) {
+            viewModel.deleteHero(new HeroDbModel(hero.getId()));
+        } else {
+            viewModel.insertHero(new HeroDbModel(hero.getId()));
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == null) return;
+
+        switch (view.getId()) {
+            case R.id.iv_fav:
+                setFav();
+                break;
+        }
+    }
 }
