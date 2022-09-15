e138901b1a97f40b06718a683a49d57542be8fae
merenapp <equijadaac@gmail.com>
2022-09-15 23:45:02 +0200

    Controlar problemas de conectividad

--- app/src/main/java/com/example/prueba001/repository/HeroRepository.kt
+++ app/src/main/java/com/example/prueba001/repository/HeroRepository.kt
@@ -2,6 +2,7 @@ package com.example.prueba001.repository
 
 import com.example.prueba001.model.HeroModel
 import com.example.prueba001.provider.HeroProvider
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 interface HeroRepository {
@@ -15,10 +16,13 @@ class HeroRepositoryImpl @Inject constructor(
     private var heros: List<HeroModel> = emptyList()
 
     override suspend fun getHeros(): List<HeroModel> {
-        val apiResponse = heroProvider.getAll().body()
-        // TODO GESTION DE ERRORES
-        heros = apiResponse ?: emptyList()
-        return heros
+        try {
+            val apiResponse = heroProvider.getAll().body()
+            heros = apiResponse ?: emptyList()
+            return heros
+        } catch(e: UnknownHostException) {
+            return emptyList()
+        }
     }
 
 
