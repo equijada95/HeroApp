e2e1955cb2537591cb6dcd4b54d1499338a25d98
merenapp <equijadaac@gmail.com>
2022-09-15 18:51:38 +0200

    Creado HeroDao

---
+++ app/src/main/java/com/example/prueba001/bbdd/dao/HeroDao.kt
@@ -0,0 +1,20 @@
+package com.example.prueba001.bbdd.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.prueba001.bbdd.models.HeroDbModel
+
+
+@Dao
+interface HeroDao {
+
+    @Query("SELECT * FROM hero_list")
+    suspend fun getAll(): LiveData<List<HeroDbModel?>?>?
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(hero: HeroDbModel?)
+
+    @Delete
+    suspend fun delete(hero: HeroDbModel?)
+
+}
