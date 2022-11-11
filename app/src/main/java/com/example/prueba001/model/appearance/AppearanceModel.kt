7cada05fdbee8a53b02d2f81eed054375e91f9c3
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2022-11-11 09:58:58 +0100

    [EXTENSIONS] Use funcion as String's extension

--- app/src/main/java/com/example/prueba001/model/appearance/AppearanceModel.kt
+++ app/src/main/java/com/example/prueba001/model/appearance/AppearanceModel.kt
@@ -1,7 +1,7 @@
 package com.example.prueba001.model.appearance
 
 import com.example.prueba001.bbdd.models.appearance.AppearanceDbModel
-import com.example.prueba001.utils.StringUtils
+import com.example.prueba001.utils.getList
 
 data class AppearanceModel(
     var gender: String,
@@ -19,8 +19,8 @@ data class AppearanceModel(
                 AppearanceModel(
                     it.gender,
                     it.race,
-                    StringUtils.getListFromString(it.height),
-                    StringUtils.getListFromString(it.weight),
+                    it.height.getList(),
+                    it.weight.getList(),
                     it.eyeColor,
                     it.hairColor
                 )
