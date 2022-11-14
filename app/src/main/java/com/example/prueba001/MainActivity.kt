3fc2b620efc9db3284423654d4a1ae3249022e37
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2022-11-14 17:14:12 +0100

    [COMPOSE] Composable inside fragment

--- app/src/main/java/com/example/prueba001/MainActivity.kt
+++ app/src/main/java/com/example/prueba001/MainActivity.kt
@@ -2,18 +2,12 @@ package com.example.prueba001
 
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
-import com.example.prueba001.composable.Detail
+import com.example.prueba001.composable.DetailFragment
 import com.example.prueba001.databinding.ActivityMainBinding
 import com.example.prueba001.fragments.list.ListFragment
 import com.example.prueba001.model.HeroModel
-import com.example.prueba001.ui.theme.HeroAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -32,17 +26,11 @@ class MainActivity : AppCompatActivity() {
     fun openDetail(hero: HeroModel) {
         actionBar = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
-        setContent {
-            HeroAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Detail(hero)
-                }
-            }
-        }
+        val transaction = supportFragmentManager.beginTransaction()
+        val fr = DetailFragment.newInstance(hero)
+        transaction.add(binding.container.id, fr)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 
     private fun inflateView() {
--- app/src/main/java/com/example/prueba001/composable/DetailComposable.kt
+++ app/src/main/java/com/example/prueba001/composable/DetailComposable.kt
@@ -1,18 +1,21 @@
 package com.example.prueba001.composable
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -20,15 +23,43 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.fragment.app.Fragment
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.prueba001.R
 import com.example.prueba001.bbdd.viewmodel.DataBaseViewModel
 import com.example.prueba001.model.HeroModel
 import com.example.prueba001.model.mapToDb
 import com.skydoves.landscapist.glide.GlideImage
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailFragment : Fragment() {
+
+    private lateinit var hero : HeroModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Detail(hero = hero)
+            }
+        }
+    }
+
+    companion object {
+        fun newInstance(hero: HeroModel): DetailFragment {
+            val fr = DetailFragment()
+            fr.hero = hero
+            return fr
+        }
+    }
+
+}
 
 @Composable
-fun Detail(
+private fun Detail(
     hero: HeroModel,
     viewModel: DataBaseViewModel = hiltViewModel()
 ) {
@@ -58,11 +89,14 @@ private fun DetailView(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(dimensionResource(id = R.dimen.padding_constraint))
+     //       .padding(dimensionResource(id = R.dimen.padding_constraint))
             .verticalScroll(rememberScrollState())
+            .background(Color.White)
     ) {
         Card(
-            elevation = dimensionResource(id = R.dimen.elevation_cardview)
+            elevation = dimensionResource(id = R.dimen.elevation_cardview),
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.padding_constraint))
         ) {
             Column(
                 modifier = Modifier
@@ -101,7 +135,7 @@ private fun DetailView(
         hero.appearance?.let { appearance ->
             Card(
                 modifier = Modifier
-                    .padding(vertical = dimensionResource(id = R.dimen.margin_cardview)), // esto actua de margin, ya que en Compose no hay margin como tal
+                    .padding(dimensionResource(id = R.dimen.padding_constraint)),
                 elevation = dimensionResource(id = R.dimen.elevation_cardview)
             ) {
                 Column(
@@ -129,7 +163,9 @@ private fun DetailView(
             }
         }
         Card(
-            elevation = dimensionResource(id = R.dimen.elevation_cardview)
+            elevation = dimensionResource(id = R.dimen.elevation_cardview),
+            modifier = Modifier
+                .padding(dimensionResource(id = R.dimen.padding_constraint))
         ) {
             Column(
                 modifier = Modifier
