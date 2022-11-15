98225747eda2f4aff3bf918fa91275152af5ea5c
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2022-11-15 13:54:44 +0100

    [COMPOSE] Created LoadingComposable

---
+++ app/src/main/java/com/example/prueba001/composable/LoadingComposable.kt
@@ -0,0 +1,31 @@
+package com.example.prueba001.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LiveDataLoadingComponent() {
+    // Column is a composable that places its children in a vertical sequence. You
+    // can think of it similar to a LinearLayout with the vertical orientation.
+    // In addition we also pass a few modifiers to it.
+
+    // You can think of Modifiers as implementations of the decorators pattern that are
+    // used to modify the composable that its applied to. In this example, we configure the
+    // Column composable to occupy the entire available width and height using
+    // Modifier.fillMaxSize().
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // A pre-defined composable that's capable of rendering a circular progress indicator. It
+        // honors the Material Design specification.
+        CircularProgressIndicator(modifier = Modifier.wrapContentWidth(Alignment.CenterHorizontally))
+    }
+}
