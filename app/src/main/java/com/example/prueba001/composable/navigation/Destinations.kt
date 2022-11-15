6fd3cb437c85f66e1230d7a30962022ed79d57ba
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2022-11-15 14:01:42 +0100

    [COMPOSE] Create Destinations

---
+++ app/src/main/java/com/example/prueba001/composable/navigation/Destinations.kt
@@ -0,0 +1,8 @@
+package com.example.prueba001.composable.navigation
+
+sealed class Destinations(
+    val route: String
+) {
+    object List: Destinations("List")
+    object Detail: Destinations("Detail")
+}
