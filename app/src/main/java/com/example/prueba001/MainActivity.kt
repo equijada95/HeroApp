fa2d5061b7607988cfc99078ce29000bdba03edc
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2022-11-15 14:45:33 +0100

    [COMPOSE] Simplify MainActivity

--- app/src/main/java/com/example/prueba001/MainActivity.kt
+++ app/src/main/java/com/example/prueba001/MainActivity.kt
@@ -1,48 +1,20 @@
 package com.example.prueba001
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.example.prueba001.bbdd.models.HeroDbModel
-import com.example.prueba001.bbdd.viewmodel.DataBaseViewModel
-import com.example.prueba001.composable.detail.DetailFragment
 import com.example.prueba001.composable.navigation.NavigationController
-import com.example.prueba001.databinding.ActivityMainBinding
-import com.example.prueba001.model.HeroModel
-import com.example.prueba001.utils.getHeroFromFavorites
-import com.example.prueba001.viewModels.HeroViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private var actionBar: ActionBar? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        inflateView()
-        openList()
-    }
-
-    fun openDetail(hero: HeroModel) {
-        actionBar = supportActionBar
-        actionBar?.setDisplayHomeAsUpEnabled(true)
-        val transaction = supportFragmentManager.beginTransaction()
-        val fr = DetailFragment.newInstance(hero)
-        transaction.add(binding.container.id, fr)
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
-
-    private fun inflateView() {
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContent {
+            NavigationController()
+        }
     }
 
     private fun openList() {
@@ -63,15 +35,5 @@ class MainActivity : AppCompatActivity() {
     //            it.isFavorite = true
     //        }
     //    }
-
-        setContent {
-            NavigationController()
-        }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        actionBar?.setDisplayHomeAsUpEnabled(false)
-        supportFragmentManager.popBackStack()
-        return true
     }
 }
