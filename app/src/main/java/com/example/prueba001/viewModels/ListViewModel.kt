80667944ef22cbd43f77443aeaea9d77cfd8a4da
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2022-11-22 13:40:16 +0100

    [STATE-FLOW] Use StateFlow instead LiveData

--- app/src/main/java/com/example/prueba001/viewModels/ListViewModel.kt
+++ app/src/main/java/com/example/prueba001/viewModels/ListViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.prueba001.viewModels
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.prueba001.bbdd.models.HeroDbModel
@@ -11,11 +10,10 @@ import com.example.prueba001.utils.mapToModel
 import com.example.prueba001.utils.setListWithFavorites
 import com.example.prueba001.viewModels.state.ListState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import java.net.SocketTimeoutException
 import javax.inject.Inject
@@ -26,21 +24,28 @@ class ListViewModel @Inject constructor(
     private val dataBaseRepository: DataBaseRepository
 ) : ViewModel() {
 
-    private val originalHeroes = MutableLiveData<List<HeroModel>>()
+    private val originalHeroes : MutableStateFlow<List<HeroModel>> = MutableStateFlow(emptyList())
 
-    private val favorites = dataBaseRepository.getHeroesFromDataBase()
+    private val favoritesFlow = dataBaseRepository.getHeroesFromDataBase()
+
+    private val coroutineScope = CoroutineScope(Job())
+
+    private lateinit var favorites: StateFlow<List<HeroDbModel>>
 
     private val _state = MutableStateFlow(ListState())
 
     val state: StateFlow<ListState> get() = _state.asStateFlow()
 
     init {
-        getHeroes()
-        favorites.observeForever {
-            var heroes = _state.value.heroList
-            heroes = setHeroesWithFavorites(heroes)
-            _state.update { it.copy(heroList = it.heroList) }
-            originalHeroes.postValue(heroes)
+        viewModelScope.launch {
+            favorites = favoritesFlow.stateIn(scope = coroutineScope)
+            getHeroes()
+            favorites.collect {
+                var heroes = _state.value.heroList
+                heroes = setHeroesWithFavorites(heroes)
+                _state.update { it.copy(heroList = it.heroList) }
+                originalHeroes.update { heroes }
+            }
         }
     }
 
@@ -59,50 +64,42 @@ class ListViewModel @Inject constructor(
     fun refresh() {
         viewModelScope.launch(Dispatchers.IO) {
             _state.update { it.copy(refreshing = true) }
-            _getHeroes()
+            getHeroes()
             _state.update { it.copy(refreshing = false) }
         }
     }
 
     fun search(search: String?) {
         search?.let { text ->
-            val searchHeros = originalHeroes.value?.filter { hero ->
+            val searchHeros = originalHeroes.value.filter { hero ->
                 hero.name.uppercase().contains(text.uppercase())
             }
-            searchHeros?.let { _state.update { it.copy(heroList = searchHeros) } }
+            _state.update { it.copy(heroList = searchHeros) }
         }
     }
 
     fun refreshSearch(search: String?) {
         viewModelScope.launch(Dispatchers.IO) {
             _state.update { it.copy(refreshing = true) }
-            _getHeroes()
+            getHeroes()
             search(search)
             _state.update { it.copy(refreshing = false) }
         }
     }
 
-    private fun getHeroes() {
-        viewModelScope.launch(Dispatchers.IO) {
-            _getHeroes()
-        }
-    }
-
-    private suspend fun _getHeroes() {
+    private suspend fun getHeroes() {
         try {
             var heroes = heroRepository.getHeroes()
 
             heroes = setHeroesWithFavorites(heroes)
             _state.update { it.copy(heroList = heroes) }
-            originalHeroes.postValue(heroes)
+            originalHeroes.update { heroes }
         } catch (_: SocketTimeoutException) { }
     }
 
     private fun setHeroesWithFavorites(heroList: List<HeroModel>): List<HeroModel> {
-        favorites.value?.let { favs ->
-            if (heroList.isEmpty()) return favs.mapToModel()
-            else heroList.setListWithFavorites(favs)
-        }
+        if (heroList.isEmpty()) return favorites.value.mapToModel()
+        else heroList.setListWithFavorites(favorites.value)
         return heroList
     }
 
