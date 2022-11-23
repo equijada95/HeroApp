d5e3a4746919342d15cc9428cbe29b87f2e7aca8
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2022-11-23 19:47:08 +0100

    [ARCHITECTURE] Optimize imports

--- domain/src/main/java/com/equijada95/heroapp/domain/module/DomainModule.kt
+++ domain/src/main/java/com/equijada95/heroapp/domain/module/DomainModule.kt
@@ -1,9 +1,6 @@
 package com.equijada95.heroapp.domain.module
 
-import android.app.Application
 import com.equijada95.heroapp.data.api.provider.HeroProvider
-import com.equijada95.heroapp.data.bbdd.dao.HeroDao
-import com.equijada95.heroapp.data.bbdd.dataBase.HeroDataBase
 import com.equijada95.heroapp.domain.api.repository.HeroRepository
 import com.equijada95.heroapp.domain.api.repository.HeroRepositoryImpl
 import com.equijada95.heroapp.domain.bbdd.repository.DataBaseRepository
@@ -12,11 +9,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.HttpUrl
-import okhttp3.HttpUrl.Companion.toHttpUrl
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
