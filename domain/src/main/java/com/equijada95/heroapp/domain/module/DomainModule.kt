55182c1375c0a17ef628f32c44369dbe2f7eda25
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2022-11-23 19:45:55 +0100

    [ARCHITECTURE] Reorganize modules

--- domain/src/main/java/com/equijada95/heroapp/domain/module/DomainModule.kt
+++ domain/src/main/java/com/equijada95/heroapp/domain/module/DomainModule.kt
@@ -1,8 +1,13 @@
 package com.equijada95.heroapp.domain.module
 
 import android.app.Application
+import com.equijada95.heroapp.data.api.provider.HeroProvider
 import com.equijada95.heroapp.data.bbdd.dao.HeroDao
 import com.equijada95.heroapp.data.bbdd.dataBase.HeroDataBase
+import com.equijada95.heroapp.domain.api.repository.HeroRepository
+import com.equijada95.heroapp.domain.api.repository.HeroRepositoryImpl
+import com.equijada95.heroapp.domain.bbdd.repository.DataBaseRepository
+import com.equijada95.heroapp.domain.bbdd.repository.DataBaseRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,5 +22,16 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class DomainModule {
+
+    @Provides
+    @Singleton
+    fun providesDataBaseRepository(dataBaseRepositoryImpl: DataBaseRepositoryImpl): DataBaseRepository {
+        return dataBaseRepositoryImpl
+    }
+
+    @Provides
+    @Singleton
+    fun providerHeroRepository(provider: HeroProvider): HeroRepository =
+        HeroRepositoryImpl(provider)
 
 }
