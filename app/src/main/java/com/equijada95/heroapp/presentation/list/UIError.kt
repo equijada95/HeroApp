ae2c94e611dfe10e834f3602db1111a20b2a8ebb
Eduardo Quijada Acevedo <equijadaac@gmail.com>
2023-01-16 18:46:02 +0100

    [UI_ERROR] Created UIError

---
+++ app/src/main/java/com/equijada95/heroapp/presentation/list/UIError.kt
@@ -0,0 +1,6 @@
+package com.equijada95.heroapp.presentation.list
+
+sealed class UIError(val messageId: Int? = null) {
+    class Error(messageId: Int): UIError(messageId)
+    class NoError(): UIError()
+}
